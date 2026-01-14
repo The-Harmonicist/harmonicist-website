92c7b6e9618ddb691e377c1066fb263b9f6d2d5d
The-Harmonicist <diazangel784@yahoo.com>
2026-01-13 22:46:43 -0800

    Added album slideshow, smooth scroll navigation, business section with Fiverr link

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
        <NavBar />
-      < Hero/>
+      < Hero />
       <Music />
       <About />
         <Contact />
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,6 +1,7 @@
 import './Contact.css'
-import { FaSpotify, FaYoutube, FaFacebook, FaInstagram, FaTiktok, FaXTwitter, FaSoundcloud, FaSteam, FaDiscord } from 'react-icons/fa6'
+import { FaSpotify, FaYoutube, FaFacebook, FaInstagram, FaTiktok, FaXTwitter, FaSoundcloud, FaSteam, FaDiscord} from 'react-icons/fa6'
 import imageName from './assets/The-Harmonicist.png'
+import { SiFiverr } from 'react-icons/si'
 export default function Contact (){
     return (
         <section className='contact' id="contact">
@@ -15,6 +16,11 @@ export default function Contact (){
             <a href='https://soundcloud.com/user-619787001' target='_blank'><FaSoundcloud /></a>
             <a href='https://steamcommunity.com/id/THarmonicist' target='_blank'><FaSteam /></a>
             <a href='https://discordapp.com/users/the_harmonicist' target='_blank'><FaDiscord /></a>
+            </div>
+            <div className="business-section">
+                <h3>Business Inquiries</h3>
+                <p>Email: <a href='mailto:agmultimediastudio@yahoo.com'>agmultimediastudio@yahoo.com</a></p>
+                <p>Hire me on <a href="https://www.fiverr.com/s/429o8dd"><SiFiverr /></a> </p>
             </div>
             <img className='contact-img' src={imageName} alt='harmonica' />
         </section>
